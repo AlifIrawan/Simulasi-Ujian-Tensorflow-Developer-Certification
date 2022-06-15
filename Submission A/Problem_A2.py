ee894010e19e06a4ec65406174215618618628b2
Madhasuki <madha@student.untan.ac.id>
2022-06-16 06:56:49 +0700

    Finish Sub A Prob B

--- Submission A/Problem_A2.py
+++ Submission A/Problem_A2.py
@@ -1,8 +1,8 @@
 # =====================================================================================
-# PROBLEM A2 
+# PROBLEM A2
 #
 # Build a Neural Network Model for Horse or Human Dataset.
-# The test will expect it to classify binary classes. 
+# The test will expect it to classify binary classes.
 # Your input layer should accept 150x150 with 3 bytes color as the input shape.
 # Don't use lambda layers in your model.
 #
@@ -17,7 +17,6 @@ import tensorflow as tf
 import os
 from keras_preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import RMSprop
-
 
 def solution_A2():
     data_url_1 = 'https://github.com/dicodingacademy/assets/releases/download/release-horse-or-human/horse-or-human.zip'
@@ -35,17 +34,47 @@ def solution_A2():
 
 
     TRAINING_DIR = 'data/horse-or-human'
+    VALIDATION_DIR = 'data/validation-horse-or-human'
     train_datagen = ImageDataGenerator(
-        # YOUR CODE HERE)
+        rescale=1./255,
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
 
-    
-    train_generator = # YOUR CODE HERE
+
+    train_generator = train_datagen.flow_from_directory(
+        TRAINING_DIR,
+        target_size=(150, 150),
+        batch_size=100,
+        class_mode='binary')
+
+    validation_datagen = ImageDataGenerator(rescale=1./255)
+    validation_generator = validation_datagen.flow_from_directory(
+        VALIDATION_DIR,
+        target_size=(150, 150),
+        batch_size=100,
+        class_mode='binary')
 
     model = tf.keras.models.Sequential([
-        # YOUR CODE HERE, end with a Neuron Dense, activated by sigmoid
-                tf.keras.layers.Dense(1, activation='sigmoid')
-        ])
+        tf.keras.layers.Conv2D(8, (3,3), activation='relu', input_shape=(150, 150, 3)),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(16, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2,2),
+        tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2,2),
+        tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2,2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
 
+    model.compile(optimizer=RMSprop(learning_rate=0.001), loss='binary_crossentropy', metrics=['accuracy'])
+    model.fit(train_generator,validation_data=validation_generator, epochs=20, verbose=2)
 
     return model
 
